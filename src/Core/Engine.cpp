976a9460d638faf5714efd3595185c89b249aac2
Jcdiem <jcdiem@protonmail.com>
2020-08-12 11:20:34 -0500

    Moving to CLion

---
+++ src/Core/Engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jzorld on 8/12/20.
+//
+
+#include "Engine.h"
---
+++ src/Core/Engine.h
@@ -0,0 +1,14 @@
+//
+// Created by jzorld on 8/12/20.
+//
+
+#ifndef PROJECT2DTD_ENGINE_H
+#define PROJECT2DTD_ENGINE_H
+
+
+class Engine {
+
+};
+
+
+#endif //PROJECT2DTD_ENGINE_H
