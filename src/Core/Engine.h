eecfd6673e31bf3a9c687d7c037f1160d04f977b
JLP <jcdiem@protonmail.com>
2020-08-12 12:03:36 -0500

    Working on engine beginnings
    Woop

--- src/Core/Engine.h
+++ src/Core/Engine.h
@@ -1,13 +1,27 @@
-//
-// Created by jzorld on 8/12/20.
-//
-
 #ifndef PROJECT2DTD_ENGINE_H
 #define PROJECT2DTD_ENGINE_H
-
+#include "SDL2/SDL.h"
+#include "stdio.h"
 
 class Engine {
+public:
+    Engine();
+    ~Engine();
 
+    void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+    bool running() {
+        return isRunning;
+    }
+
+private:
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer  *renderer;
 };
 
 
