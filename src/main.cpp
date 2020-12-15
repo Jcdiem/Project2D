648a6672858469f7618a967697f0ead8be37558f
Kodi <cam.peterson@protonmail.com>
2020-12-15 15:13:01 -0600

    Added some flags for fun!
    Added help and debug flags, try them out (:

--- src/main.cpp
+++ src/main.cpp
@@ -21,26 +21,55 @@ void print_exception(const std::exception& e, int level =  0)
 }
 
 int main (int argc, char* argv[]) {
+    //FLAGS
+    bool debug = false;
+    //FLAGS
+
+
     srand((int)time(0));//Just for multi cd demo
+
+    for(int i = 1; i < argc; i++) {
+        printf("%s: ", argv[i]);
+        //ADD ARGS!
+        if(strcmp(argv[i], "--debug") == 0 || strcmp(argv[i], "-d") == 0 ) {
+            printf("Debug enabled!\n");
+            debug = true;
+        } else if(strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0 ) {
+            printf("Showing Help!\n");
+            printf("The Following flags are available:\n");
+            printf("--debug \\ -d | Enable debug mode\n");
+            printf("--help  \\ -h | Show this help message\n");
+            std::exit(0);
+        } else {
+            printf("Unknown flag.\n");
+        }
+
+    }
+
     try {
         SDL_Thread *renderingThread;
         int threadReturn;
 
-        const int TPS = 30; //! Target TPS
-                            //! Different from FPS, this is the cycle speed of main loop, 30 cap is good,
-                            //! adjusting this number affects game speed
-        const int frameDelay = 1000000000 / TPS; //expected frame delay in ns
-        std::cout << "Goal frametime: " << frameDelay << std::endl;
+        const int TPS = 120; //! Target TPS
+        //! Different from FPS, this is the cycle speed of main loop, 30 cap is good,
+        //! adjusting this number affects game speed
+        const int frameDelay = 1000000000 / TPS; //expected frame time in ns
+        if (debug) {
+            std::cout << "Goal frametime: " << frameDelay / 1000000.0 << "ms"<< std::endl;
+        }
 
         int frameTime;
+
+        int timeLost = 0;
+
         engine = new Engine();
 
         engine->init(&engine->getJson()->getTitle()[0],SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,engine->getJson()->getXSize(),engine->getJson()->getYSize(),false, false);
 
         renderingThread = SDL_CreateThread(renderThread, "RenderThread", nullptr);
-        //Check to make sure thread isn't rart
+
         if (nullptr == renderingThread) {
-            printf("SDL_CreateThread failed: %s\n", SDL_GetError());
+            std::throw_with_nested(SDL_GetError());
         }
 
         while(engine->running()) {
@@ -53,15 +82,38 @@ int main (int argc, char* argv[]) {
             auto frameEnd = std::chrono::steady_clock::now();
             frameTime = std::chrono::duration_cast<std::chrono::nanoseconds>(frameEnd-frameStart).count();
 //          Frame debugging
-            std::cout << "Time for frame was: " << frameTime / 1000000.0 << "ms" << std::endl;
+            if(debug) {
+                std::cout << "Time for frame was: " << frameTime / 1000000.0 << "ms" << std::endl;
+            }
 
             if(frameDelay > frameTime && frameTime != 0){
                 using namespace std::chrono_literals;
-//                SDL_Delay(frameDelay/frameTime);
-                std::this_thread::sleep_for(1ns * (frameDelay/frameTime));
+                if(timeLost == 0) {
+                    int sleepTime = frameDelay - frameTime;
+                    if(debug) {
+                        std::cout << "Sleep time        = " << sleepTime / 1000000.0 << "ms" << std::endl;
+                        std::cout << "Total time        = " << sleepTime + frameTime << "ms, (Should be same as goal frametime)" << std::endl;
+                    }
+                    std::this_thread::sleep_for(1ns * (sleepTime));
+                } else {
+                    int timeSaved = frameDelay - frameTime;
+                    timeLost -= timeSaved;
+                    if(debug) {
+                        std::cout << timeSaved / 1000000.0 << "ms saved" << std::endl;
+                        std::cout << "Currently making up lost time! " << timeLost / 1000000.0 << "ms to make up!" << std::endl;
+                    }
+                    if(timeLost < 0) {
+                        std::this_thread::sleep_for(1ns * abs(timeLost));
+                        timeLost = 0;
+                        if(debug) {
+                            std::cout << "No more time to make up!" << std::endl;
+                        }
+                    }
+                }
             } else {
-                float lostFrames = frameTime / frameDelay;
+                float lostFrames = float(frameTime) / frameDelay;
                 std::cout << "Uh oh, engine thread can't keep up! About " << lostFrames << " frames behind..." << std::endl;
+                timeLost += (frameTime - frameDelay);
             }
         }
 
