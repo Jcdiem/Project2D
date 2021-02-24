c8c6738ee0fbc959273fb5440110f551be2edfe2
Kodi <cam.peterson@protonmail.com>
2021-02-24 14:29:04 -0600

    Revert splitting Script Component
    Template Programming requires it all to be in header? /:

--- src/ECS/ScriptComponent.h
+++ src/ECS/ScriptComponent.h
@@ -10,18 +10,89 @@
 
 class ScriptComponent : public Component{
 public:
-    ScriptComponent(Manager* manager, char* path);
-    ScriptComponent(Manager* manager, char* path, int x, int y);
+    ScriptComponent(Manager* manager, char* path) {
+        xPos = 0;
+        yPos = 0;
+        this->path = path;
+        this->manager = manager;
+        engineW = manager->getWW();
+        engineH = manager->getWH();
 
-    void init() override;
-    float x();
-    float y();
-    int getEW();
-    int getEH();
-    void setPos(float x, float y);
-    void setX(float x);
-    void setY(float y);
-    void update() override;
+        std::ifstream t(path);
+
+        //Finding the end of file
+        t.seekg(0, std::ios::end);
+        script.reserve(t.tellg());
+        t.seekg(0, std::ios::beg);
+        //Finding the end of file
+
+        script.assign((std::istreambuf_iterator<char>(t)),
+                      std::istreambuf_iterator<char>());
+    }
+
+    ScriptComponent(Manager* manager, char* path, int x, int y) {
+        xPos = x;
+        yPos = y;
+        this->path = path;
+        this->manager = manager;
+        engineW = manager->getWW();
+        engineH = manager->getWH();
+
+        std::ifstream t(path);
+
+        //Finding the end of file
+        t.seekg(0, std::ios::end);
+        script.reserve(t.tellg());
+        t.seekg(0, std::ios::beg);
+        //Finding the end of file
+
+        script.assign((std::istreambuf_iterator<char>(t)),
+                      std::istreambuf_iterator<char>());
+    }
+
+    void init() {
+        lua.gLu()->set_function("x", &ScriptComponent::x, this);
+        lua.gLu()->set_function("y", &ScriptComponent::y, this);
+        lua.gLu()->set_function("setX", &ScriptComponent::setX, this);
+        lua.gLu()->set_function("setY", &ScriptComponent::setY, this);
+        lua.gLu()->set_function("EW", &ScriptComponent::getEW, this);
+        lua.gLu()->set_function("EH", &ScriptComponent::getEH, this);
+
+        lua.initScript(script);
+    }
+
+    float x() {
+        return xPos;
+    }
+
+    float y() {
+        return yPos;
+    }
+
+    int getEW() {
+        return engineW;
+    }
+
+    int getEH() {
+        return engineH;
+    }
+
+    void setPos(float x, float y) {
+        xPos = x;
+        yPos = y;
+    }
+
+    void setX(float x) {
+        xPos = x;
+    }
+
+    void setY(float y) {
+        yPos = y;
+    }
+
+    void update() {
+        lua.runScript();
+    }
 
 private:
     float xPos;
