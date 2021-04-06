7f54e6d55142a97570559d9acc20d3799fca3a10
kodirex <kodi@kodirex.space>
2021-04-05 23:14:21 -0500

    Strange bug?
    Did some work on main engine class. Give it a look and help me figure out the issue?

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,45 +1,102 @@
+#include <iostream>
 #include "Engine.h"
 
-Engine::Engine() = default;
-
-Engine::~Engine() = default;
-
-//Engine Globals (PRIVATE)
-sf::CircleShape testCircle;
-
-void Engine::init(const char *title, int width, int height,
-                  bool fullscreen, bool resizable, int threads) {
-    window = new sf::RenderWindow(sf::VideoMode(width,height),"Project 2D");
-
-
-    testCircle = sf::CircleShape(100.f);
-    testCircle.setFillColor(sf::Color::Green);
+Engine::Engine(const std::string& title, int width, int height, bool fullscreen, int threads) {
+    if(fullscreen) {
+        window = new sf::RenderWindow(sf::VideoMode(width,height),title, sf::Style::Fullscreen);
+    } else {
+        window = new sf::RenderWindow(sf::VideoMode(width,height),title, sf::Style::Close);
+    }
 
     isRunning = true;
 }
 
+Engine::~Engine() { //Destructor used to destroy the sfml window and join any threads that need joining.
+    window->close();
 
-//void Engine::handleEvents() {
-//    SDL_PumpEvents();
-//}
+    for(std::thread& system : systems) {
+        system.join();
+    }
+}
+
+void Engine::initSystem(Systems system, int tickrate) {  //Used to init some or all engine systems, error handling may go here.
+    switch(system) {
+        case event_listener: //Hijacks the current thread to run the listener function (via clock runner)
+            clockRunner(&Engine::listen, tickrate);
+            break;
+        case engine_update: //Calls the clock runner function (as a new thread) with a function ptr to update.
+            systems[0] = std::thread(&Engine::clockRunner, this, &Engine::update, tickrate);
+            break;
+        case engine_render: //Calls the clock runner function (as a new thread) with a function ptr to render.
+            systems[1] = std::thread(&Engine::clockRunner, this, &Engine::render, tickrate);
+            break;
+        case engine_all: //Calls the clock runner function (as a new thread) with a function ptr to update and render, then hijacks the thread for the listener.
+            systems[0] = std::thread(&Engine::clockRunner, this, &Engine::update, tickrate);
+            systems[1] = std::thread(&Engine::clockRunner, this, &Engine::render, tickrate);
+            clockRunner(&Engine::listen, tickrate);
+            break;
+    }
+}
+
+void Engine::listen() { //Used to handle various sfml events, currently only exits, but could be more!
+    switch (event.type) {
+        case sf::Event::Closed: //The issue lies here, it seems the program receives an exit event constantly for some reason?
+            quit();
+            break;
+        default:
+            break;
+    }
+}
 
 void Engine::update() {
-//    manager.refresh();
-//    manager.update(threads);
+    //Manager calls
 }
 
 void Engine::render() {
+    //Render calls
     window->clear();
 
-    //window->draw things go here
-    window->draw(testCircle);
+    //Render things here!
 
     window->display();
 }
 
-void Engine::clean() {
+bool Engine::running() const {
+    return isRunning;
 }
 
-void Engine::quit() {
+void Engine::quit() { //Handles exit operations that need to be done before the destructor is called to finish the job.
     isRunning = false;
+}
+
+void Engine::clockRunner(void (Engine::*system)(), int tickrate) { //This function calls the Engine function supplied [tickrate] times per second, and attempts to account for lag.
+    const uint64_t tickDelay = 1000000000 / tickrate; //expected frame time in ns
+    u_int64_t tickTime;
+    uint64_t timeLost = 0;
+
+    while(isRunning) {
+        auto tickStart = std::chrono::steady_clock::now();
+
+        (this->*system)();
+
+        auto tickEnd = std::chrono::steady_clock::now();
+        tickTime = std::chrono::duration_cast<std::chrono::nanoseconds>(tickEnd-tickStart).count();
+
+        if(tickDelay > tickTime && tickTime != 0){
+            using namespace std::chrono_literals;
+            if(timeLost == 0) {
+                uint64_t sleepTime = tickDelay - tickTime;
+                std::this_thread::sleep_for(1ns * (sleepTime));
+            } else {
+                uint64_t timeSaved = tickDelay - tickTime;
+                timeLost -= timeSaved;
+                if(timeLost < 0) {
+                    std::this_thread::sleep_for(-1ns * timeLost);
+                    timeLost = 0;
+                }
+            }
+        } else {
+            timeLost += (tickTime - tickDelay);
+        }
+    }
 }
--- src/Engine.h
+++ src/Engine.h
@@ -1,43 +1,42 @@
 #pragma once
 #ifndef PROJECT2DTD_ENGINE_H
 #define PROJECT2DTD_ENGINE_H
-#include <SFML/Graphics.hpp>
-#include <chrono>
-#include <random>
-#include <iostream>
-#include <thread>
-#include <vector>
 
-class TextureHandler;
+#include <SFML/Graphics.hpp>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+enum Systems {
+    event_listener = 0,
+    engine_update = 1,
+    engine_render = 2,
+    engine_all = 3
+};
 
 class Engine {
 public:
-    Engine();
+    Engine(const std::string& title, int width, int height, bool fullscreen, int threads);
     ~Engine();
 
+    void initSystem(Systems system, int tickrate); //Takes a enum to select which system to init
 
-    void init(const char* title, int width, int height, bool fullscreen, bool resizable, int threads);
-
-    //void loadEntity(EntityType* entity);
-
-    void handleEvents();
+    void listen();
     void update();
     void render();
-    void clean();
+    [[nodiscard]] bool running() const;
+
     void quit();
 
-    bool running() const {
-        return isRunning;
-    }
-
+    void clockRunner(void (Engine::*x)(), int tickrate);
 private:
-    bool isRunning{};
-    sf::RenderWindow *window;
-    std::vector<sf::Drawable*> drawQueue;
-    int threads;
+    std::thread systems[2];
 
-//    Manager manager;
-    std::vector<std::string> levelList;
+    bool isRunning;
+
+    sf::Event event;
+    sf::RenderWindow* window;
 };
 
 #endif //PROJECT2DTD_ENGINE_H
